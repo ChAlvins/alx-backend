4cd614ec9c3e664d8252f2fdcb3b41e91bf44311
vpnchengo <charo.alvin@gmail.com>
2023-08-22 14:14:15 +0300

    fifo caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""containing FIFICache module"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """defining the lass"""
+    def __init__(self):
+        """initializing class"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data
+        the item value for the key"""
+        if key is None or item is None:
+            pass
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            oldest_key = self.queue.pop(0)
+            self.cache_data.pop(oldest_key)
+            print('DISCARD: {}\n'.format(oldest_key))
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """return value in self.cache_data linked to key"""
+        if key is None or Key not in self.cache_data:
+            return None
+        return self.cache_data[key]
