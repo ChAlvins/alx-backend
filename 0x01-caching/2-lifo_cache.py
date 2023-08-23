e5586ead94037ff9044ffd62a8cd943b7dabc1ab
vpnchengo <charo.alvin@gmail.com>
2023-08-23 12:20:50 +0300

    update

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,16 +16,18 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             pass
         else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            cache_length = len(self.cache_data)
+            base_items = BaseCaching.MAX_ITEMS
+            if cache_length >= base_items and key not in self.cache_data:
                 newest_key = self.queue.pop()
                 self.cache_data.pop(newest_key)
-                print('DISCARD: {}\n'.format(newest_key))
+                print('DISCARD: {}'.format(newest_key))
 
             self.queue.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
         """return value in self.cache_data linked to key"""
-        if key is None or key not in self.cache_data:
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
