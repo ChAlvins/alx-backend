33141cca0d1be5433c842695fe41003c283c8ef6
vpnchengo <charo.alvin@gmail.com>
2023-08-23 12:05:20 +0300

    update

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -28,6 +28,6 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """return value in self.cache_data linked to key"""
-        if key is None and key not in self.cache_data.keys():
+        if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
