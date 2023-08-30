a1713ad90a7979f2b7e23ddcffeacd196dc2ec0b
vpnchengo <charo.alvin@gmail.com>
2023-08-30 12:03:43 +0300

    updated

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,12 +18,13 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """ configure supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """for the / route"""
+    """renders the / route"""
     return render_template('2-index.html')
 
 
